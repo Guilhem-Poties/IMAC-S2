0745f138752977ede26e018320fc92e2c4a8462d
Guilhem <gpoties@gmail.com>
2024-03-29 12:04:07 +0100

    TD03 Exercice 2

--- TD03/src/main.cpp
+++ TD03/src/main.cpp
@@ -8,7 +8,7 @@
 #include <math.h>
 
 
-enum class Operator { ADD, SUB, MUL, DIV, OPEN_PAREN, CLOSE_PAREN};
+enum class Operator { ADD, SUB, MUL, DIV, POW, OPEN_PAREN, CLOSE_PAREN, NONE };
 enum class TokenType { OPERATOR, OPERAND };
 struct Token {
   TokenType type;
@@ -16,26 +16,36 @@ struct Token {
   Operator op;
 };
 
-
-std::vector<std::string> split_string(std::string const& s)
-{
-    std::istringstream in(s); // transforme une chaîne en flux de caractères, cela simule un flux comme l'est std::cin
-    // l’itérateur va lire chaque element de "in", comme un flux d'entrée, chaque élément est séparé par un espace
-    return std::vector<std::string>(std::istream_iterator<std::string>(in), std::istream_iterator<std::string>()); 
-}
+std::vector<std::string> split_string(std::string const& s);
 
 bool is_floating(std::string const& s);
 
 float npi_evaluate(std::vector<std::string> const& tokens);
 
 
+Token makeToken(float value);
+Token makeToken(Operator op);
+
+std::vector<Token> tokenize(std::vector<std::string> const& words);
+
+float npi_evaluate(std::vector<Token> const& tokens);
+
+
 int main() {
     std::string npi {};
     // std::getline(std::cin, npi);
 
-    std::cout << npi_evaluate(split_string("3 4 2 ^ 1 5 - 6 ^ / +")) << std::endl;
+    std::cout << npi_evaluate(tokenize(split_string("3 4 2 ^ 1 5 - 6 ^ / +"))) << std::endl;
 
     return 0;
+}
+
+
+
+std::vector<std::string> split_string(std::string const& s) {
+    std::istringstream in(s); // transforme une chaîne en flux de caractères, cela simule un flux comme l'est std::cin
+    // l’itérateur va lire chaque element de "in", comme un flux d'entrée, chaque élément est séparé par un espace
+    return std::vector<std::string>(std::istream_iterator<std::string>(in), std::istream_iterator<std::string>()); 
 }
 
 bool is_floating(std::string const& s) {
@@ -47,15 +57,16 @@ bool is_floating(std::string const& s) {
     return false;
 }
 
-float operation(std::string operator_, float leftOperand, float rightOperand) {
+float operation(std::string op, float leftOperand, float rightOperand) {
     float result {};
-    if (operator_ == "+") result = leftOperand + rightOperand;
-    else if (operator_ == "-") result = leftOperand - rightOperand;
-    else if (operator_ == "*") result = leftOperand * rightOperand;
-    else if (operator_ == "/") result = leftOperand / rightOperand;
-    else if (operator_ == "^") result = std::pow(leftOperand, rightOperand);
+    if (op == "+") result = leftOperand + rightOperand;
+    else if (op == "-") result = leftOperand - rightOperand;
+    else if (op == "*") result = leftOperand * rightOperand;
+    else if (op == "/") result = leftOperand / rightOperand;
+    else if (op == "^") result = std::pow(leftOperand, rightOperand);
+    else result = (float)NULL;
 
-    // std::cout << leftOperand << rightOperand << operator_;
+    // std::cout << leftOperand << rightOperand << op;
     
     return result;
 }
@@ -78,4 +89,73 @@ float npi_evaluate(std::vector<std::string> const& tokens) {
         }
     }
     return values.top();
+}
+
+
+Token makeToken(float value) {
+    return {TokenType::OPERAND, value, Operator::NONE};
+}
+Token makeToken(Operator op) {
+    return {TokenType::OPERATOR, (float)NULL, op};
+}
+
+Operator findOpFromStr(std::string s) {
+    Operator result {};
+    if (s == "+") result = Operator::ADD;
+    else if (s == "-") result = Operator::SUB;
+    else if (s == "*") result = Operator::MUL;
+    else if (s == "/") result = Operator::DIV;
+    else if (s == "^") result = Operator::POW;    
+    else if (s == "(") result = Operator::OPEN_PAREN;
+    else if (s == ")") result = Operator::CLOSE_PAREN;
+    else result = Operator::NONE;
+
+    return result;
+}
+
+std::vector<Token> tokenize(std::vector<std::string> const& words) {
+    std::vector<Token> tokenList {};
+    for (size_t i = 0; i < words.size(); i++) {
+        if (is_floating(words[i])) {
+            tokenList.push_back(makeToken(stof(words[i])));
+        }
+        else {
+            tokenList.push_back(makeToken(findOpFromStr(words[i])));
+        }
+    }
+    return tokenList;
+}
+
+float operation(Operator op, float leftOperand, float rightOperand) {
+    float result {};
+    if (op == Operator::ADD) result = leftOperand + rightOperand;
+    else if (op == Operator::SUB) result = leftOperand - rightOperand;
+    else if (op == Operator::MUL) result = leftOperand * rightOperand;
+    else if (op == Operator::DIV) result = leftOperand / rightOperand;
+    else if (op == Operator::POW) result = std::pow(leftOperand, rightOperand);
+    else result = (float)NULL;
+
+    // std::cout << leftOperand << rightOperand << op;
+    
+    return result;
+}
+
+float npi_evaluate(std::vector<Token> const& tokens) {
+    std::stack<float> values;
+    for (size_t i = 0; i < tokens.size(); i++) {
+        if (tokens[i].type == TokenType::OPERAND) {
+            values.push(tokens[i].value);
+        }
+        else {
+            float rightOperand {values.top()};
+            values.pop();
+            float leftOperand {values.top()};
+            values.pop();
+
+            values.push(operation(tokens[i].op, leftOperand, rightOperand));
+            
+            // std::cout << leftOperand << tokens[i] << rightOperand;
+        }
+    }
+    return values.top();    
 }
