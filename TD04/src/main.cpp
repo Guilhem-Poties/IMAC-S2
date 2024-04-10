74b1d20a450ff8a3fe49a1fe514eb80d32879203
Guilhem <gpoties@gmail.com>
2024-04-10 12:35:25 +0200

    TD04 Exercice 2

--- TD04/src/main.cpp
+++ TD04/src/main.cpp
@@ -1,18 +1,24 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <numeric> 
+#include <numeric>
+#include <string>
+
+int sizeWord(std::string const& word);
+
+std::vector<std::string> split_string(std::string const& str);
 
 int main() {
+
+    /************ EXERCICE 1 ************/
+
     /* 1 */
     std::vector<int> vec {};
-
     for (int _ = 0; _ < 99; _++) {
         vec.push_back(rand() % 100);
     }
 
     std::vector<int>::iterator begin {vec.begin()};
-
     std::vector<int>::iterator end {vec.end()};
 
     /* 5 */
@@ -44,5 +50,43 @@ int main() {
     std::cout << "La somme du tableau vaut " << std::accumulate(begin, end, 0) << std::endl;
     
     
+    /************ EXERCICE 2 ************/
+
+    /* 1 */
+
+    std::cout << sizeWord("Il etait une fois") << std::endl;
+
+    for (std::string str : split_string("Il etait une fois")) std::cout << str << std::endl;
+    
     return 0;
+}
+
+int sizeWord(std::string const& word) {
+    auto const is_space = [](char letter){ return letter == ' '; };
+
+    if (auto it = std::find_if(word.begin(), word.end(), is_space); it != word.end()) {
+        return std::distance(word.begin(), it);
+    }
+    else {
+        return 0;
+    }
+}
+
+std::vector<std::string> split_string(std::string const& str) {
+    std::vector<std::string> tabStr {};
+    size_t lastWordPos {0};
+    
+    while (true) {
+        size_t size = sizeWord(str.substr(lastWordPos));
+        if (size != 0) {
+            // std::cout << lastWordPos << ", " << size << ", " << str.substr(lastWordPos) << std::endl;
+            tabStr.push_back(str.substr(lastWordPos, size));
+            lastWordPos += size + 1;
+        }
+        else {
+            tabStr.push_back(str.substr(lastWordPos));
+            break;
+        }
+    }
+    return tabStr;
 }
